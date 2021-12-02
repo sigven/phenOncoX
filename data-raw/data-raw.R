1b6c49c5e0f1d684554e545b61ff8a28a89441ba
Sigve Nakken <sigven@ifi.uio.no>
2021-12-02 23:40:27 +0100

    0.2.6 release

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -1,5 +1,5 @@
 library(magrittr)
-source('R/functions.R')
+source('data-raw/functions.R')
 
 disease_ontology_release <- 'v2021-11-17'
 efo_release <- 'v3.36.0'
@@ -7,7 +7,7 @@ oncotree_release <- '2021_11_02'
 
 ## Get UMLS / DiseaseOntology / EFO mappings
 umls_map <- map_umls(
-  update = T,
+  update = F,
   basedir = here::here())
 
 
