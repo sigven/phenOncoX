1dfdd601e0cc93ea9ddf1eda1c077b18e931135a
Sigve Nakken <sigven@ifi.uio.no>
2025-09-24 08:44:08 +0200

    fix sysdata

--- data-raw/data-raw.R
+++ data-raw/data-raw.R
@@ -73,7 +73,7 @@ db[['oncotree_core']] <- oncotree_core
 db[['oncotree_expanded']] <- oncotree_expanded
 db[['auxiliary_maps']] <- auxiliary_maps
 
-version_bumped <- "1.0.1"
+version_bumped <- "1.0.2"
 gd_records <- list()
 db_id_ref <- data.frame()
 
